31cf25d01248d07cf4d06060c79f149e07a5bcf0
maybetree <maybetree48@proton.me>
2025-07-27 16:29:48 +0200

    git-annex in maybetree@mistilteinn:~/rsyncdata/data/proj/treeup

--- homedirs/mistilteinn-screen-layout/.config/X11/xinitrc.d/20-screen-layout.sh
+++ homedirs/mistilteinn-screen-layout/.config/X11/xinitrc.d/20-screen-layout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --mode 1440x900 --pos 1920x900 --rotate right --output DP-3 --primary --mode 1920x1080 --pos 0x2160 --rotate normal --output HDMI-1 --mode 1920x1080 --pos 0x1080 --rotate normal
+xrandr --output DP-1 --primary --mode 1920x1080 --pos 0x2160 --rotate normal --output DP-2 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-3 --mode 1440x900 --pos 1920x942 --rotate right --output HDMI-1 --mode 1920x1080 --pos 0x1080 --rotate normal
