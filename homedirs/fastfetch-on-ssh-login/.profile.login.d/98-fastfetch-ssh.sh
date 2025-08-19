c08a2a25b2a1905e189e42cdd382888e7caa1870
maybetree <maybetree48@proton.me>
2025-08-19 18:06:05 +0200

    autocommit Tue Aug 19 18:06:05 CEST 2025

--- homedirs/fastfetch-on-ssh-login/.profile.login.d/98-fastfetch-ssh.sh
+++ homedirs/fastfetch-on-ssh-login/.profile.login.d/98-fastfetch-ssh.sh
@@ -1,5 +1,6 @@
 
-if [ -n "$SSH_CONNECTION" ]; then
+if [ -n "$SSH_CONNECTION" ] &&  [ -z "$SSH_LOGIN_FETCH" ]; then
 	(pfetch || pfetch-rs || fastfetch || neofetch || screenfetch) 2>/dev/null
+	export SSH_LOGIN_FETCH="done"
 fi
 
