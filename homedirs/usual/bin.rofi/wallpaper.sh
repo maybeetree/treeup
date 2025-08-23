16200c3bfc93131b0543fb0dc984dfc9fb69d4db
maybetree <maybetree48@proton.me>
2025-08-23 10:18:43 +0200

    fix wallpaper script for annex

--- homedirs/usual/bin.rofi/wallpaper.sh
+++ homedirs/usual/bin.rofi/wallpaper.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-MONS=$(find ~/proj/pictures-i-like/wallpapers -type f -not -path '*xfce*' -not -name '*.webp' | shuf)
+# -not -d instead of -f is because they are all symlinks when managed by
+# git-annex
+MONS=$(find ~/proj/pictures-i-like/wallpapers -not -type d -not -path '*xfce*' -not -name '*.webp' | shuf)
 j=1
 
 for i in $(xrandr | grep " connected" | cut -d' ' -f 1)
