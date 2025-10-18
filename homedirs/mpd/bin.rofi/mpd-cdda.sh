4c3d5c837bee0e2aecdae9455e9984dacdc88aaf
maybetree <maybetree48@proton.me>
2025-10-18 05:26:33 +0200

    git-annex in maybetree@mistilteinn:~/rsyncdata/data/proj/treeup

---
+++ homedirs/mpd/bin.rofi/mpd-cdda.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mpc insert cdda://
+
--- homedirs/usual/bin.rofi/xinput_map_to_output.sh
+++ homedirs/usual/bin.rofi/xinput_map_to_output.sh
@@ -1,4 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
-xinput map-to-output "$(xinput --list --name-only | dmenu -l 100)" "$(xrandr | grep ' connected' | cut -d ' ' -f 1 | dmenu -l 100)"
+sleep 1
+
+choice1="$(xinput --list --name-only | dmenu -i)"
+
+if [ -z "$choice1" ]
+then
+	exit 1
+fi
+
+choice2="$(xrandr | grep ' connected' | cut -d ' ' -f 1 | dmenu -i)"
+
+if [ -z "$choice2" ]
+then
+	exit 1
+fi
+
+xinput map-to-output "$choice1" "$choice2"
 
